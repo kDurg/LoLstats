d08217bb2b4d60d3cf8bf27e7669e8546274f74c
Kyle Durigan <kyle.durigan@gmail.com>
2019-10-16 20:41:19 -0600

    Updated server layout

--- server/server.js
+++ server/server.js
@@ -1,50 +1,35 @@
-if (process.env.NODE_ENV !== 'production') {
-	console.log('loading dev environments')
-	require('dotenv').config()
-}
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const session = require('express-session')
-const app = express()
-const PORT = process.env.PORT || 8080
+require('dotenv').config();
 
-// ===== Middleware ====
-app.use(morgan('dev'))
-app.use(
-	bodyParser.urlencoded({
-		extended: false
-	})
-)
-app.use(bodyParser.json())
-app.use(
-	session({
-		secret: process.env.APP_SECRET || 'this is the default passphrase',
-		store: new MongoStore({ mongooseConnection: dbConnection }),
-		resave: false,
-		saveUninitialized: false
-	})
-)
+const app = express();
+const port = process.env.PORT || 5000;
 
-// ==== if its production environment!
-if (process.env.NODE_ENV === 'production') {
-	const path = require('path')
-	console.log('YOU ARE IN THE PRODUCTION ENV')
-	app.use('/static', express.static(path.join(__dirname, '../build/static')))
-	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, '../build/'))
-	})
-}
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-app.use(function(err, req, res, next) {
-	console.log('====== ERROR =======')
-	console.error(err.stack)
-	res.status(500)
-})
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
 
-// ==== Starting Server =====
-app.listen(PORT, () => {
-	console.log(`App listening on PORT: ${PORT}`)
-})
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
+
+// http://stayregular.net/blog/make-a-nodejs-api-with-mysql
+// https://www.youtube.com/watch?v=7CqJlxBYj-M
